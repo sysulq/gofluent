1edf3d460d0e33c979202148c1a648a260ebe8f2
sophos <hnlq.sysu@gmail.com>
2014-10-28 08:20:27 +0000

    support glob match tag

--- glob.go
+++ glob.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"path"
+	"regexp"
 	"strings"
 )
 
@@ -77,4 +78,98 @@ func Glob(fs http.FileSystem, pattern string) ([]string, error) {
 			return nil
 		},
 	})
+}
+
+type PatternError struct {
+	message string
+}
+
+func (self *PatternError) Error() string {
+	return self.message
+}
+
+func buildRegexpFromGlobPatternInner(pattern string, startPos int) (string, int, error) {
+	state := 0
+	chunk := ""
+
+	patternLength := len(pattern)
+	var i int
+	for i = startPos; i < patternLength; i += 1 {
+		if state == 0 {
+			c := pattern[i]
+			if c == '*' {
+				state = 2
+			} else if c == '}' || c == ',' {
+				break
+			} else if c == '{' {
+				chunk += "(?:"
+				first := true
+				for {
+					i += 1
+					subchunk, lastPos, err := buildRegexpFromGlobPatternInner(pattern, i)
+					if err != nil {
+						return "", 0, err
+					}
+					if lastPos == patternLength {
+						return "", 0, &PatternError{"unexpected end of pattern (in expectation of '}' or ',')"}
+					}
+					if !first {
+						chunk += "|"
+					}
+					i = lastPos
+					chunk += "(?:" + subchunk + ")"
+					first = false
+					if pattern[lastPos] == '}' {
+						break
+					} else if pattern[lastPos] != ',' {
+						return "", 0, &PatternError{"never get here"}
+					}
+				}
+				chunk += ")"
+			} else {
+				chunk += regexp.QuoteMeta(string(c))
+			}
+		} else if state == 1 {
+			// escape
+			c := pattern[i]
+			chunk += regexp.QuoteMeta(string(c))
+			state = 0
+		} else if state == 2 {
+			c := pattern[i]
+			if c == '*' {
+				state = 3
+			} else {
+				chunk += "[^.]*" + regexp.QuoteMeta(string(c))
+				state = 0
+			}
+		} else if state == 3 {
+			// recursive any
+			c := pattern[i]
+			if c == '*' {
+				return "", 0, &PatternError{"unexpected *"}
+			} else if c == '.' {
+				chunk += "(?:.*\\.|^)"
+			} else {
+				chunk += ".*" + regexp.QuoteMeta(string(c))
+			}
+			state = 0
+		}
+	}
+	if state == 2 {
+		chunk += "[^.]*"
+	} else if state == 3 {
+		chunk += ".*"
+	}
+	return chunk, i, nil
+}
+
+func BuildRegexpFromGlobPattern(pattern string) (string, error) {
+	chunk, pos, err := buildRegexpFromGlobPatternInner(pattern, 0)
+	if err != nil {
+		return "", err
+	}
+	if pos != len(pattern) {
+		return "", &PatternError{"unexpected '" + string(pattern[pos]) + "'"}
+	}
+	return "^" + chunk + "$", nil
 }
