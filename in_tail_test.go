4e75fd2dfccc960b426b277522a0720bf9ae2689
sophos <hnlq.sysu@gmail.com>
2014-11-27 11:52:40 +0000

    improve test case with goconvey

--- in_tail_test.go
+++ in_tail_test.go
@@ -9,7 +9,7 @@ import (
 	"time"
 )
 
-func Test_json(t *testing.T) {
+func TestTailJson(t *testing.T) {
 	log.SetOutput(ioutil.Discard)
 	cf := make(map[string]string)
 	cf["path"] = "/tmp/test.file"
@@ -44,6 +44,51 @@ func Test_json(t *testing.T) {
 
 	Convey("Create tail file and write something", t, func() {
 		Convey("pipelinepack should get the json map", func() {
+			So(pack.Msg.Data["data"], ShouldEqual, "test")
+			So(pack.Msg.Data["hello"], ShouldEqual, "world")
+		})
+	})
+
+	os.Remove(cf["pos_file"])
+	os.Remove(cf["path"])
+}
+
+func TestTailRegex(t *testing.T) {
+	log.SetOutput(ioutil.Discard)
+	cf := make(map[string]string)
+	cf["path"] = "/tmp/test.file"
+	cf["format"] = "/^(?P<data>.*), (?P<hello>.*)$/"
+	cf["tag"] = "test"
+	cf["pos_file"] = "/tmp/test.pos"
+	tail := new(inputTail)
+
+	Convey("Init tail plugin", t, func() {
+		err := tail.Init(cf)
+		Convey("tail.Init err should be nil", func() {
+			So(err, ShouldEqual, nil)
+		})
+	})
+
+	rChan := make(chan *PipelinePack)
+	InputRecycleChan := make(chan *PipelinePack, 1)
+	iPack := NewPipelinePack(InputRecycleChan)
+	InputRecycleChan <- iPack
+	iRunner := NewInputRunner(InputRecycleChan, rChan)
+	os.Remove(cf["pos_file"])
+	os.Remove(cf["path"])
+
+	f, _ := os.OpenFile(cf["path"], os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0600)
+
+	go tail.Run(iRunner)
+	time.Sleep(1 * time.Second)
+	f.Write([]byte("test, world\n"))
+	f.Close()
+
+	pack := <-iRunner.RouterChan()
+
+	Convey("Create tail file and write something", t, func() {
+		Convey("pipelinepack should get the regex map", func() {
+			So(pack.Msg.Tag, ShouldEqual, "test")
 			So(pack.Msg.Data["data"], ShouldEqual, "test")
 			So(pack.Msg.Data["hello"], ShouldEqual, "world")
 		})
