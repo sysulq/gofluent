552a20204e47ae6f6e4fa6a40d06630680ce0630
dw_liqi1 <dw_liqi1@7ef3bcf2-fb9a-444a-897f-2debf83a3c19>
2014-12-08 07:32:56 +0000

    git-svn-id: svn://172.19.108.123/yysec/trunk/gofluent@2638 7ef3bcf2-fb9a-444a-897f-2debf83a3c19

--- bench.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-i="0"
-
-while [ $i -lt 100000 ]
-do
-  echo '{"data":"mydata","test":"mytest"}' >> /usr/local/ysec_agent/log/exec_log.json
-  i=$[$i+1]
-done
--- config_test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"testing"
-)
-
-func Test_ReadConf(t *testing.T) {
-
-}
